93df0a7cf5813608d699fd1d8e9ed7c3829dc86b
AlbertIsoi <126046756+AlbertIsoi@users.noreply.github.com>
2025-07-30 10:10:44 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,78 @@
-pavel@debian:~$ mbpoll -m tcp -a 2 -r 0 -c 1 192.168.88.66
-mbpoll: start reference out of range (0) ! Try -h for help.
-pavel@debian:~$ 
-
+pavel@debian:~$ ping 192.168.88.66
+PING 192.168.88.66 (192.168.88.66) 56(84) bytes of data.
+64 bytes from 192.168.88.66: icmp_seq=1 ttl=64 time=0.018 ms
+64 bytes from 192.168.88.66: icmp_seq=2 ttl=64 time=0.020 ms
+64 bytes from 192.168.88.66: icmp_seq=3 ttl=64 time=0.024 ms
+64 bytes from 192.168.88.66: icmp_seq=4 ttl=64 time=0.030 ms
+64 bytes from 192.168.88.66: icmp_seq=5 ttl=64 time=0.039 ms
+64 bytes from 192.168.88.66: icmp_seq=6 ttl=64 time=0.045 ms
+64 bytes from 192.168.88.66: icmp_seq=7 ttl=64 time=0.042 ms
+64 bytes from 192.168.88.66: icmp_seq=8 ttl=64 time=0.044 ms
+64 bytes from 192.168.88.66: icmp_seq=9 ttl=64 time=0.043 ms
+64 bytes from 192.168.88.66: icmp_seq=10 ttl=64 time=0.044 ms
+64 bytes from 192.168.88.66: icmp_seq=11 ttl=64 time=0.047 ms
+64 bytes from 192.168.88.66: icmp_seq=12 ttl=64 time=0.045 ms
+64 bytes from 192.168.88.66: icmp_seq=13 ttl=64 time=0.044 ms
+64 bytes from 192.168.88.66: icmp_seq=14 ttl=64 time=0.047 ms
+64 bytes from 192.168.88.66: icmp_seq=15 ttl=64 time=0.046 ms
+64 bytes from 192.168.88.66: icmp_seq=16 ttl=64 time=0.045 ms
+64 bytes from 192.168.88.66: icmp_seq=17 ttl=64 time=0.044 ms
+64 bytes from 192.168.88.66: icmp_seq=18 ttl=64 time=0.042 ms
+64 bytes from 192.168.88.66: icmp_seq=19 ttl=64 time=0.034 ms
+64 bytes from 192.168.88.66: icmp_seq=20 ttl=64 time=0.048 ms
+64 bytes from 192.168.88.66: icmp_seq=21 ttl=64 time=0.049 ms
+64 bytes from 192.168.88.66: icmp_seq=22 ttl=64 time=0.045 ms
+64 bytes from 192.168.88.66: icmp_seq=23 ttl=64 time=0.044 ms
+64 bytes from 192.168.88.66: icmp_seq=24 ttl=64 time=0.045 ms
+64 bytes from 192.168.88.66: icmp_seq=25 ttl=64 time=0.044 ms
+64 bytes from 192.168.88.66: icmp_seq=26 ttl=64 time=0.047 ms
+64 bytes from 192.168.88.66: icmp_seq=27 ttl=64 time=0.045 ms
+64 bytes from 192.168.88.66: icmp_seq=28 ttl=64 time=0.049 ms
+64 bytes from 192.168.88.66: icmp_seq=29 ttl=64 time=0.048 ms
+64 bytes from 192.168.88.66: icmp_seq=30 ttl=64 time=0.040 ms
+64 bytes from 192.168.88.66: icmp_seq=31 ttl=64 time=0.045 ms
+64 bytes from 192.168.88.66: icmp_seq=32 ttl=64 time=0.044 ms
+64 bytes from 192.168.88.66: icmp_seq=33 ttl=64 time=0.043 ms
+64 bytes from 192.168.88.66: icmp_seq=34 ttl=64 time=0.046 ms
+64 bytes from 192.168.88.66: icmp_seq=35 ttl=64 time=0.046 ms
+64 bytes from 192.168.88.66: icmp_seq=36 ttl=64 time=0.049 ms
+64 bytes from 192.168.88.66: icmp_seq=37 ttl=64 time=0.046 ms
+64 bytes from 192.168.88.66: icmp_seq=38 ttl=64 time=0.044 ms
+64 bytes from 192.168.88.66: icmp_seq=39 ttl=64 time=0.049 ms
+64 bytes from 192.168.88.66: icmp_seq=40 ttl=64 time=0.044 ms
+64 bytes from 192.168.88.66: icmp_seq=41 ttl=64 time=0.046 ms
+64 bytes from 192.168.88.66: icmp_seq=42 ttl=64 time=0.044 ms
+64 bytes from 192.168.88.66: icmp_seq=43 ttl=64 time=0.044 ms
+64 bytes from 192.168.88.66: icmp_seq=44 ttl=64 time=0.046 ms
+^C
+--- 192.168.88.66 ping statistics ---
+44 packets transmitted, 44 received, 0% packet loss, time 44021ms
+rtt min/avg/max/mdev = 0.018/0.042/0.049/0.007 ms
+pavel@debian:~$ ping 192.168.88.66
+PING 192.168.88.66 (192.168.88.66) 56(84) bytes of data.
+64 bytes from 192.168.88.66: icmp_seq=1 ttl=64 time=0.019 ms
+64 bytes from 192.168.88.66: icmp_seq=2 ttl=64 time=0.020 ms
+64 bytes from 192.168.88.66: icmp_seq=3 ttl=64 time=0.025 ms
+64 bytes from 192.168.88.66: icmp_seq=4 ttl=64 time=0.032 ms
+64 bytes from 192.168.88.66: icmp_seq=5 ttl=64 time=0.041 ms
+64 bytes from 192.168.88.66: icmp_seq=6 ttl=64 time=0.044 ms
+64 bytes from 192.168.88.66: icmp_seq=7 ttl=64 time=0.049 ms
+64 bytes from 192.168.88.66: icmp_seq=8 ttl=64 time=0.047 ms
+64 bytes from 192.168.88.66: icmp_seq=9 ttl=64 time=0.049 ms
+64 bytes from 192.168.88.66: icmp_seq=10 ttl=64 time=0.060 ms
+64 bytes from 192.168.88.66: icmp_seq=11 ttl=64 time=0.030 ms
+64 bytes from 192.168.88.66: icmp_seq=12 ttl=64 time=0.165 ms
+64 bytes from 192.168.88.66: icmp_seq=13 ttl=64 time=0.056 ms
+64 bytes from 192.168.88.66: icmp_seq=14 ttl=64 time=0.047 ms
+64 bytes from 192.168.88.66: icmp_seq=15 ttl=64 time=0.079 ms
+64 bytes from 192.168.88.66: icmp_seq=16 ttl=64 time=0.038 ms
+64 bytes from 192.168.88.66: icmp_seq=17 ttl=64 time=0.056 ms
+64 bytes from 192.168.88.66: icmp_seq=18 ttl=64 time=0.048 ms
+64 bytes from 192.168.88.66: icmp_seq=19 ttl=64 time=0.046 ms
+64 bytes from 192.168.88.66: icmp_seq=20 ttl=64 time=0.049 ms
+64 bytes from 192.168.88.66: icmp_seq=21 ttl=64 time=0.048 ms
+64 bytes from 192.168.88.66: icmp_seq=22 ttl=64 time=0.049 ms
+^C
+--- 192.168.88.66 ping statistics ---
+22 packets transmitted, 22 received, 0% packet loss, time 21485ms
+rtt min/avg/max/mdev = 0.019/0.049/0.165/0.028 ms
