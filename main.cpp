bb31fd8c5309298e639b79b3cbe4b8ad37567301
AlbertIsoi <126046756+AlbertIsoi@users.noreply.github.com>
2025-07-30 09:53:30 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,25 @@
-pavel@debian:~$ sudo ip addr add 192.168.88.100/24 dev eth0
-[sudo] пароль для pavel: 
-Cannot find device "eth0"
-pavel@debian:~$ ip a
-1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default qlen 1000
-    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
-    inet 127.0.0.1/8 scope host lo
-       valid_lft forever preferred_lft forever
-    inet6 ::1/128 scope host noprefixroute 
-       valid_lft forever preferred_lft forever
-2: enp2s0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc fq_codel state UP group default qlen 1000
-    link/ether 40:8d:5c:d0:98:01 brd ff:ff:ff:ff:ff:ff
-    inet 192.168.88.66/24 brd 192.168.88.255 scope global dynamic noprefixroute enp2s0
-       valid_lft 345sec preferred_lft 345sec
-    inet 192.168.88.100/24 scope global secondary enp2s0
-       valid_lft forever preferred_lft forever
-    inet6 fe80::489:7676:bf3c:a7c3/64 scope link noprefixroute 
-       valid_lft forever preferred_lft forever
-3: wlx0857003e7951: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 qdisc mq state DOWN group default qlen 1000
-    link/ether f2:44:f1:bd:92:c4 brd ff:ff:ff:ff:ff:ff permaddr 08:57:00:3e:79:51
-4: enxc84d4429a8a0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc fq_codel state UP group default qlen 1000
-    link/ether c8:4d:44:29:a8:a0 brd ff:ff:ff:ff:ff:ff
-    inet6 fe80::ce2a:a91:c922:9296/64 scope link noprefixroute 
-       valid_lft forever preferred_lft forever
+pavel@debian:~$ mbpoll -b 115200 -t 3 -p none -r 9200 -a 3 /dev/ttyUSB0
+mbpoll 1.0-0 - FieldTalk(tm) Modbus(R) Master Simulator
+Copyright © 2015-2019 Pascal JEAN, https://github.com/epsilonrt/mbpoll
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions; type 'mbpoll -w' for details.
+
+mbpoll: Connection failed: No such file or directory.
+pavel@debian:~$ mbpoll -m tcp -a 1 -r 9200 -c 1 192.168.88.66
+mbpoll 1.0-0 - FieldTalk(tm) Modbus(R) Master Simulator
+Copyright © 2015-2019 Pascal JEAN, https://github.com/epsilonrt/mbpoll
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions; type 'mbpoll -w' for details.
+
+mbpoll: Connection failed: Connection refused.
+pavel@debian:~$ mbpoll -m tcp -a 3 -r 9200 -c 1 192.168.88.66
+mbpoll 1.0-0 - FieldTalk(tm) Modbus(R) Master Simulator
+Copyright © 2015-2019 Pascal JEAN, https://github.com/epsilonrt/mbpoll
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions; type 'mbpoll -w' for details.
+
+mbpoll: Connection failed: Connection refused.
 pavel@debian:~$ 
-
-
