7869fde67237664e8413d5874220a1159d9314ff
AlbertIsoi <126046756+AlbertIsoi@users.noreply.github.com>
2025-07-30 10:42:55 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -100,5 +100,186 @@ pavel@debian:~$  ip a
 5: wlx0857003e7951: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 qdisc mq state DOWN group default qlen 1000
     link/ether 4a:20:ab:53:e1:36 brd ff:ff:ff:ff:ff:ff permaddr 08:57:00:3e:79:51
 pavel@debian:~$ 
-pavel@debian:~$ 
-
+pavel@debian:~$ sudo ip addr flush dev enp2s0
+pavel@debian:~$ sudo ip addr add 192.168.88.100/24 dev enp2s0
+pavel@debian:~$ sudo apt install nmap
+Чтение списков пакетов… Готово
+Построение дерева зависимостей… Готово
+Чтение информации о состоянии… Готово         
+Уже установлен пакет nmap самой новой версии (7.93+dfsg1-1).
+Следующие пакеты устанавливались автоматически и больше не требуются:
+  linux-image-6.1.0-25-amd64 linux-image-6.1.0-27-amd64
+Для их удаления используйте «sudo apt autoremove».
+Обновлено 0 пакетов, установлено 0 новых пакетов, для удаления отмечено 0 пакетов, и 7 пакетов не обновлено.
+pavel@debian:~$ nmap -sP 192.168.88.0/24
+Starting Nmap 7.93 ( https://nmap.org ) at 2025-07-30 10:40 +05
+Nmap scan report for 192.168.88.1
+Host is up (0.00054s latency).
+Nmap scan report for 192.168.88.2
+Host is up (0.043s latency).
+Nmap scan report for 192.168.88.3
+Host is up (0.0035s latency).
+Nmap scan report for 192.168.88.4
+Host is up (0.0066s latency).
+Nmap scan report for 192.168.88.5
+Host is up (0.0034s latency).
+Nmap scan report for 192.168.88.6
+Host is up (0.0033s latency).
+Nmap scan report for 192.168.88.10
+Host is up (0.0013s latency).
+Nmap scan report for 192.168.88.11
+Host is up (0.0018s latency).
+Nmap scan report for 192.168.88.13
+Host is up (0.0013s latency).
+Nmap scan report for 192.168.88.22
+Host is up (0.054s latency).
+Nmap scan report for 192.168.88.30
+Host is up (0.00091s latency).
+Nmap scan report for 192.168.88.39
+Host is up (0.0015s latency).
+Nmap scan report for 192.168.88.40
+Host is up (0.0015s latency).
+Nmap scan report for 192.168.88.41
+Host is up (0.00087s latency).
+Nmap scan report for 192.168.88.42
+Host is up (0.00083s latency).
+Nmap scan report for 192.168.88.43
+Host is up (0.0022s latency).
+Nmap scan report for 192.168.88.44
+Host is up (0.0022s latency).
+Nmap scan report for 192.168.88.45
+Host is up (0.0025s latency).
+Nmap scan report for 192.168.88.46
+Host is up (0.0014s latency).
+Nmap scan report for 192.168.88.47
+Host is up (0.0025s latency).
+Nmap scan report for 192.168.88.48
+Host is up (0.0046s latency).
+Nmap scan report for 192.168.88.49
+Host is up (0.0012s latency).
+Nmap scan report for 192.168.88.50
+Host is up (0.0011s latency).
+Nmap scan report for 192.168.88.51
+Host is up (0.0011s latency).
+Nmap scan report for 192.168.88.52
+Host is up (0.0013s latency).
+Nmap scan report for 192.168.88.54
+Host is up (0.0026s latency).
+Nmap scan report for 192.168.88.57
+Host is up (0.0021s latency).
+Nmap scan report for 192.168.88.73
+Host is up (0.0025s latency).
+Nmap scan report for 192.168.88.100
+Host is up (0.00019s latency).
+Nmap scan report for 192.168.88.108
+Host is up (0.0022s latency).
+Nmap scan report for 192.168.88.131
+Host is up (0.0015s latency).
+Nmap scan report for 192.168.88.142
+Host is up (0.0012s latency).
+Nmap scan report for 192.168.88.148
+Host is up (0.00081s latency).
+Nmap scan report for 192.168.88.149
+Host is up (0.00077s latency).
+Nmap scan report for 192.168.88.158
+Host is up (0.0011s latency).
+Nmap scan report for 192.168.88.188
+Host is up (0.0011s latency).
+Nmap scan report for 192.168.88.216
+Host is up (0.0016s latency).
+Nmap scan report for 192.168.88.228
+Host is up (0.0011s latency).
+Nmap scan report for 192.168.88.236
+Host is up (0.0041s latency).
+Nmap scan report for 192.168.88.240
+Host is up (0.089s latency).
+Nmap scan report for 192.168.88.243
+Host is up (0.0020s latency).
+Nmap done: 256 IP addresses (41 hosts up) scanned in 19.26 seconds
+pavel@debian:~$ nmap -sP 192.168.88.0/24
+Starting Nmap 7.93 ( https://nmap.org ) at 2025-07-30 10:41 +05
+Nmap scan report for 192.168.88.1
+Host is up (0.00047s latency).
+Nmap scan report for 192.168.88.3
+Host is up (0.0017s latency).
+Nmap scan report for 192.168.88.4
+Host is up (0.0020s latency).
+Nmap scan report for 192.168.88.5
+Host is up (0.0016s latency).
+Nmap scan report for 192.168.88.6
+Host is up (0.0019s latency).
+Nmap scan report for 192.168.88.8
+Host is up (0.0072s latency).
+Nmap scan report for 192.168.88.10
+Host is up (0.0023s latency).
+Nmap scan report for 192.168.88.11
+Host is up (0.00097s latency).
+Nmap scan report for 192.168.88.13
+Host is up (0.0047s latency).
+Nmap scan report for 192.168.88.22
+Host is up (0.094s latency).
+Nmap scan report for 192.168.88.30
+Host is up (0.00035s latency).
+Nmap scan report for 192.168.88.39
+Host is up (0.00063s latency).
+Nmap scan report for 192.168.88.40
+Host is up (0.00078s latency).
+Nmap scan report for 192.168.88.41
+Host is up (0.0027s latency).
+Nmap scan report for 192.168.88.42
+Host is up (0.00048s latency).
+Nmap scan report for 192.168.88.43
+Host is up (0.00051s latency).
+Nmap scan report for 192.168.88.44
+Host is up (0.0028s latency).
+Nmap scan report for 192.168.88.45
+Host is up (0.0013s latency).
+Nmap scan report for 192.168.88.46
+Host is up (0.0012s latency).
+Nmap scan report for 192.168.88.47
+Host is up (0.0024s latency).
+Nmap scan report for 192.168.88.48
+Host is up (0.0013s latency).
+Nmap scan report for 192.168.88.49
+Host is up (0.0012s latency).
+Nmap scan report for 192.168.88.50
+Host is up (0.0011s latency).
+Nmap scan report for 192.168.88.51
+Host is up (0.00051s latency).
+Nmap scan report for 192.168.88.52
+Host is up (0.0022s latency).
+Nmap scan report for 192.168.88.54
+Host is up (0.00070s latency).
+Nmap scan report for 192.168.88.57
+Host is up (0.00059s latency).
+Nmap scan report for 192.168.88.73
+Host is up (0.00086s latency).
+Nmap scan report for 192.168.88.100
+Host is up (0.00018s latency).
+Nmap scan report for 192.168.88.108
+Host is up (0.00053s latency).
+Nmap scan report for 192.168.88.131
+Host is up (0.0025s latency).
+Nmap scan report for 192.168.88.142
+Host is up (0.0026s latency).
+Nmap scan report for 192.168.88.148
+Host is up (0.0044s latency).
+Nmap scan report for 192.168.88.149
+Host is up (0.00098s latency).
+Nmap scan report for 192.168.88.158
+Host is up (0.0037s latency).
+Nmap scan report for 192.168.88.180
+Host is up (0.16s latency).
+Nmap scan report for 192.168.88.188
+Host is up (0.0031s latency).
+Nmap scan report for 192.168.88.216
+Host is up (0.00075s latency).
+Nmap scan report for 192.168.88.228
+Host is up (0.0018s latency).
+Nmap scan report for 192.168.88.236
+Host is up (0.00088s latency).
+Nmap scan report for 192.168.88.240
+Host is up (0.088s latency).
+Nmap scan report for 192.168.88.243
+Host is up (0.0012s latency).
+Nmap done: 256 IP addresses (42 hosts up) scanned in 25.01 seconds
