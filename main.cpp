7636a9560fbab46bbf2af77f81e9ab55df9167d5
AlbertIsoi <126046756+AlbertIsoi@users.noreply.github.com>
2025-07-30 11:40:48 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,48 +1,39 @@
-pavel@debian:~$ sudo ip addr flush dev enp2s0
 pavel@debian:~$ sudo ip addr add 192.168.88.100/24 dev enp2s0
-pavel@debian:~$ ping 192.168.88.88]
-ping: 192.168.88.88]: Неизвестное имя или служба
+pavel@debian:~$ ip a show enp2s0
+2: enp2s0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc fq_codel state UP group default qlen 1000
+    link/ether 40:8d:5c:d0:98:01 brd ff:ff:ff:ff:ff:ff
+    inet 192.168.88.100/24 scope global enp2s0
+       valid_lft forever preferred_lft forever
 pavel@debian:~$ ping 192.168.88.88
 PING 192.168.88.88 (192.168.88.88) 56(84) bytes of data.
-From 192.168.88.100 icmp_seq=1 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=2 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=3 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=4 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=5 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=6 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=7 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=8 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=9 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=10 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=11 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=12 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=13 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=14 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=15 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=16 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=17 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=18 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=19 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=20 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=21 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=22 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=23 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=24 Destination Host Unreachable
-^C
---- 192.168.88.88 ping statistics ---
-27 packets transmitted, 0 received, +24 errors, 100% packet loss, time 26610ms
-pipe 4
-pavel@debian:~$ ping 192.168.88.88
-PING 192.168.88.88 (192.168.88.88) 56(84) bytes of data.
-From 192.168.88.100 icmp_seq=1 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=2 Destination Host Unreachable
-From 192.168.88.100 icmp_seq=3 Destination Host Unreachable
-^C
---- 192.168.88.88 ping statistics ---
-5 packets transmitted, 0 received, +3 errors, 100% packet loss, time 4087ms
-pipe 4
 pavel@debian:~$ sudo nmap -p 502 192.168.88.88
-Starting Nmap 7.93 ( https://nmap.org ) at 2025-07-30 11:17 +05
-Note: Host seems down. If it is really up, but blocking our ping probes, try -Pn
-Nmap done: 1 IP address (0 hosts up) scanned in 1.49 seconds
+[sudo] пароль для pavel: 
+Starting Nmap 7.93 ( https://nmap.org ) at 2025-07-30 11:34 +05
+setup_target: failed to determine route to 192.168.88.88
+WARNING: No targets were specified, so 0 hosts scanned.
+Nmap done: 0 IP addresses (0 hosts up) scanned in 0.03 seconds
+pavel@debian:~$ ip addr show enp2s0
+2: enp2s0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc fq_codel state UP group default qlen 1000
+    link/ether 40:8d:5c:d0:98:01 brd ff:ff:ff:ff:ff:ff
+    inet6 fe80::489:7676:bf3c:a7c3/64 scope link noprefixroute 
+       valid_lft forever preferred_lft forever
+pavel@debian:~$ sudo nmap -Pn -p 502 192.168.88.88
+Starting Nmap 7.93 ( https://nmap.org ) at 2025-07-30 11:37 +05
+mass_dns: warning: Unable to determine any DNS servers. Reverse DNS is disabled. Try using --system-dns or specify valid servers with --dns-servers
+Nmap scan report for 192.168.88.88
+Host is up (0.00022s latency).
+
+PORT    STATE SERVICE
+502/tcp open  mbap
+MAC Address: 58:A1:5F:29:E4:1D (Unknown)
+
+Nmap done: 1 IP address (1 host up) scanned in 0.17 seconds
+pavel@debian:~$ mbpoll -m tcp -a 2 -r 0 -c 1 192.168.88.88
+mbpoll: start reference out of range (0) ! Try -h for help.
 pavel@debian:~$ 
+
+
+
+
+
+
